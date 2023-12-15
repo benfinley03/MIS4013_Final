5d83f7d7b022d7843ce239f694a1c2aaa33383bf
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 03:08:07 -0600

    Update classesModel.php

--- model/classesModel.php
+++ model/classesModel.php
@@ -12,4 +12,49 @@ function selectClasses() {
         throw $e;
     }
 }
+
+
+function insertClass($class) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `classes` (`class`) VALUES (?)");
+        $stmt->bind_param("s", $class);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateClass($class, $class_id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `classes` SET `class`=? WHERE TypeID = ?");
+        $stmt->bind_param("si", $class, $class_id);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteClass($class_id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `classes` WHERE `class_id` = ?");
+        $stmt->bind_param("i", $class_id);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
 ?>
