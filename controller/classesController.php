0636378eaa2f3bc5d651070a9e407868ec598f4b
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 23:12:18 -0600

    Update classesController.php

--- controller/classesController.php
+++ controller/classesController.php
@@ -1,9 +1,9 @@
 <?php
-//require_once("connection.php");
+require_once("/home/benfinmi/repositories/MIS4013_Final/connection.php");
 require_once("/home/benfinmi/repositories/MIS4013_Final/model/classesModel.php");
 $pageTitle = "Classes";
-include "Other/view-header.php";
+include "/home/benfinmi/repositories/MIS4013_Final/Other/view-header.php";
 $classes = selectClasses();
-include "view/classesView.php";
-include "Other/view-footer.php";
+include "/home/benfinmi/repositories/MIS4013_Final/view/classesView.php";
+include "/home/benfinmi/repositories/MIS4013_Final/Other/view-footer.php";
 ?>
