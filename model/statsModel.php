694b8ca98e5378b5dd5511cc842f0d8dc1a42cb6
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 23:55:48 -0600

    Update statsModel.php

--- model/statsModel.php
+++ model/statsModel.php
@@ -1,1 +1,18 @@
+<?php
+function selectCharacterStats() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT s.*, c.name, c.class
+                                FROM stats s
+                                  JOIN characters c ON s.stats_char_id = c.character_id");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
 
