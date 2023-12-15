ef356da6148f65977ef9f4adf6efeb619c7ad9f2
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 22:28:34 -0600

    Update classesView.php

--- view/classesView.php
+++ view/classesView.php
@@ -1,1 +1,13 @@
+<!-- Inside classesView.php -->
 
+<h1>All Classes</h1>
+
+<?php if ($moves->num_rows > 0) : ?>
+    <ul>
+        <?php while ($row = $moves->fetch_assoc()) : ?>
+            <li><?php echo $row['class']; ?></li>
+        <?php endwhile; ?>
+    </ul>
+<?php else : ?>
+    <p>No classes available.</p>
+<?php endif; ?>
