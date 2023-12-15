d309679f68772b5b3b2527b567e622045240f899
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 23:16:10 -0600

    Update racesModel.php

--- model/racesModel.php
+++ model/racesModel.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectRaces() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT race_id, race FROM races;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
