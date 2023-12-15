98cd9f7cfc489e3e3d782134d2fb24296d9a63da
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 23:39:51 -0600

    Update view-header.php

--- Other/view-header.php
+++ Other/view-header.php
@@ -1,28 +1,16 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?=$pageTitle?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
-  </head>
-  <body>
-    <div class = "containers">
-      <nav class="navbar navbar-expand-lg bg-body-tertiary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-      <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-        <a class="nav-link" href="controller/classesController.php">Classes</a>
-        <a class="nav-link" href="controller/racesController.php">Races</a>
-        <a class="nav-link" href="controller/charactersController.php">Characters</a>
-        <a class="nav-link" href="controller/statsController.php">Stats</a>
-
-      </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/index.php">My Character Database</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item"><a class="nav-link" href="/index.php">Home</a></li>
+                <li class="nav-item"><a class="nav-link" href="/controllers/classesController.php">Classes</a></li>
+                <li class="nav-item"><a class="nav-link" href="/controllers/racesController.php">Races</a></li>
+                <li class="nav-item"><a class="nav-link" href="/controllers/charactersController.php">Characters</a></li>
+            </ul>
+        </div>
     </div>
-  </div>
 </nav>
