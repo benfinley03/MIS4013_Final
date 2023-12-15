d6a1d264ff5ccafcc2a64fb577d351e7ed582c14
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 08:22:34 -0600

    Update view-header.php

--- Other/view-header.php
+++ Other/view-header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title><?php echo $pageTitle; ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+Wy2nFgA5om8IbbVdJzazt+JX6p0a4U9Bp" crossorigin="anonymous">
 
 
