3ea1b433e790ca1904b4bfc531ccd98c7a2ca049
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 22:25:04 -0600

    Update classesController.php

--- controller/classesController.php
+++ controller/classesController.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once("Other/util-db.php");
+require_once("model/classesModel.php");
+$pageTitle = "Classes";
+include "Other/view-header.php";
+$moves = selectClasses();
+include "view/classesView.php";
+include "Other/view-footer.php";
+?>
