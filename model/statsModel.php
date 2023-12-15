05e9a2c260b0908ed1716b8373fca44fe161fe0e
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 01:34:31 -0600

    Update statsModel.php

--- model/statsModel.php
+++ model/statsModel.php
@@ -1,14 +1,9 @@
 <?php
-function selectStats($cid) {
+function selectStats() {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT s.*, c.name, cl.class 
-                                FROM stats s 
-                                    JOIN characters c ON s.stats_char_id = c.character_id 
-                                    JOIN classes cl ON c.char_class_id = cl.class_id
-                                WHERE c.character_id = ?");
-
-        $stmt->bind_param("i", $cid);
+        $stmt = $conn->prepare("SELECT *
+                                FROM stats");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
