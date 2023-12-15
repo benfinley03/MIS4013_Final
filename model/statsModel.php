6c4e2344406ca497c3994dc4c084b3ab7f3788d6
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 02:21:17 -0600

    Update statsModel.php

--- model/statsModel.php
+++ model/statsModel.php
@@ -4,7 +4,8 @@ function selectStatsWithCharacters() {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT c.*, s.*
                                 FROM characters c
-                                LEFT JOIN stats s ON c.character_id = s.stats_char_id");
+                                LEFT JOIN stats s ON c.character_id = s.stats_char_id
+                                JOIN classes cl ON c.char_class_id = cl.class_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
