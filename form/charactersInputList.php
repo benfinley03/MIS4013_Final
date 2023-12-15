128e50578279a9fc02ac1c9d9e2935310d070ea5
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 04:12:20 -0600

    Update charactersInputList.php

--- form/charactersInputList.php
+++ form/charactersInputList.php
@@ -1,9 +1,26 @@
+<label for="character_id" class="form-label">Character</label>
 <select class="form-select" id="character_id" name="character_id">
-<?php
-while ($charItem = $charList->fetch_assoc()) {
-?>
-    <option value="<?php echo $charItem['character_id']; ?>"><?php echo $charItem['name']; ?></option>
-<?php
-}
-?>
+    <?php
+    while ($charItem = $charList->fetch_assoc()) {
+        echo '<option value="' . $charItem['character_id'] . '">' . $charItem['name'] . '</option>';
+    }
+    ?>
+</select>
+
+<label for="race_id" class="form-label">Race</label>
+<select class="form-select" id="race_id" name="race_id">
+    <?php
+    while ($raceItem = $raceList->fetch_assoc()) {
+        echo '<option value="' . $raceItem['race_id'] . '">' . $raceItem['race'] . '</option>';
+    }
+    ?>
+</select>
+
+<label for="class_id" class="form-label">Class</label>
+<select class="form-select" id="class_id" name="class_id">
+    <?php
+    while ($classItem = $classList->fetch_assoc()) {
+        echo '<option value="' . $classItem['class_id'] . '">' . $classItem['class'] . '</option>';
+    }
+    ?>
 </select>
