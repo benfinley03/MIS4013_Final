9fdcd4d2d5a7800711322758757692eaeb16b3ca
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 02:02:13 -0600

    Update statsModel.php

--- model/statsModel.php
+++ model/statsModel.php
@@ -1,9 +1,11 @@
 <?php
-function selectStats() {
+function selectStats($characterId) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT *
-                                FROM stats");
+                                FROM stats
+                                WHERE stats_char_id = ?");
+        $stmt->bind_param("i", $characterId);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
