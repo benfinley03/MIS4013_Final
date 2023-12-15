853840926e025bad35524b820df0be04f1889b20
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 06:32:56 -0600

    Update statsNew.php

--- form/statsNew.php
+++ form/statsNew.php
@@ -54,32 +54,32 @@ $noStats = getCharactersWithoutStats();
                         <input type="integer" class="form-control" id="hit_points" name="hit_points">
                     </div>
                     <div class="mb-3">
-                        <label for="hit_points" class="form-label">Hit Points</label>
-                        <input type="integer" class="form-control" id="hit_points" name="hit_points">
+                        <label for="armor_class" class="form-label">Armor</label>
+                        <input type="integer" class="form-control" id="armor_class" name="armor_class">
                     </div>
                     <div class="mb-3">
-                        <label for="hit_points" class="form-label">Hit Points</label>
-                        <input type="integer" class="form-control" id="hit_points" name="hit_points">
+                        <label for="strength" class="form-label">Strength</label>
+                        <input type="integer" class="form-control" id="strength" name="strength">
                     </div>
                     <div class="mb-3">
-                        <label for="hit_points" class="form-label">Hit Points</label>
-                        <input type="integer" class="form-control" id="hit_points" name="hit_points">
+                        <label for="dexterity" class="form-label">Dexterity</label>
+                        <input type="integer" class="form-control" id="dexterity" name="dexterity">
                     </div>
                     <div class="mb-3">
-                        <label for="hit_points" class="form-label">Hit Points</label>
-                        <input type="integer" class="form-control" id="hit_points" name="hit_points">
+                        <label for="constitution" class="form-label">Constitution</label>
+                        <input type="integer" class="form-control" id="constitution" name="constitution">
                     </div>
                     <div class="mb-3">
-                        <label for="hit_points" class="form-label">Hit Points</label>
-                        <input type="integer" class="form-control" id="hit_points" name="hit_points">
+                        <label for="intelligence" class="form-label">Intelligence</label>
+                        <input type="integer" class="form-control" id="intelligence" name="intelligence">
                     </div>
                     <div class="mb-3">
-                        <label for="hit_points" class="form-label">Hit Points</label>
-                        <input type="integer" class="form-control" id="hit_points" name="hit_points">
+                        <label for="wisdom" class="form-label">Wisdom</label>
+                        <input type="integer" class="form-control" id="wisdom" name="wisdom">
                     </div>
                     <div class="mb-3">
-                        <label for="hit_points" class="form-label">Hit Points</label>
-                        <input type="integer" class="form-control" id="hit_points" name="hit_points">
+                        <label for="charisma" class="form-label">Charisma</label>
+                        <input type="integer" class="form-control" id="charisma" name="charisma">
                     </div>
 
                     <input type="hidden" name="actionType" value="Add">
