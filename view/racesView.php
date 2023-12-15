8edeff24efc15bb8ba581f3a775307f4374d1866
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 23:17:21 -0600

    Update racesView.php

--- view/racesView.php
+++ view/racesView.php
@@ -1,1 +1,20 @@
+<h1>Races</h1>
+<div class="table-responsive">
+    <table class="table table-bordered table-hover">
+        <thead class="thead-dark">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php while ($race = $races->fetch_assoc()) : ?>
+                <tr>
+                    <td><?php echo $race['race_id']; ?></td>
+                    <td><?php echo $race['race']; ?></td>
+                </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
+</div>
 
