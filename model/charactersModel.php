b61f36a986814c47f82d9d4f4156a67b7af49293
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 23:42:11 -0600

    Update charactersModel.php

--- model/charactersModel.php
+++ model/charactersModel.php
@@ -5,7 +5,7 @@ function selectCharacters() {
         $stmt = $conn->prepare("SELECT c.character_id, c.name, r.race, cl.class, c.level, c.alignment, c.background
                                 FROM characters c
                                   JOIN races r ON c.char_race_id = r.race_id
-                                  JOIN classes cl ON c.char_class_id = cl.class_id);
+                                  JOIN classes cl ON c.char_class_id = cl.class_id");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
