b1693c4fbfd8f9e41efa2b451780cc1aafdd9866
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 22:44:24 -0600

    Update view-header.php

--- Other/view-header.php
+++ Other/view-header.php
@@ -16,7 +16,7 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link active" aria-current="page" href="Other/index.php">Home</a>
+        <a class="nav-link active" aria-current="page" href="index.php">Home</a>
         <a class="nav-link" href="controller/classesController.php">Classes</a>
         <a class="nav-link" href="controller/racesController.php">Races</a>
         <a class="nav-link" href="controller/charactersController.php">Characters</a>
