d0ec87bb51bdb0e3d7796e38d100e261099aab91
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 06:55:33 -0600

    Update statsModel.php

--- model/statsModel.php
+++ model/statsModel.php
@@ -15,4 +15,37 @@ function selectStatsWithCharacters() {
         throw $e;
     }
 }
+
+
+function insertCharacterStats($stats_char_id, $hit_points, $armor_class, $strength, $dexterity, $constitution, $intelligence, $wisdom, $charisma) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `character_stats` (`stats_char_id`, `hit_points`, `armor_class`, `strength`, `dexterity`, `constitution`, `intelligence`, `wisdom`, `charisma`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->bind_param("iiiiiiiii", $stats_char_id, $hit_points, $armor_class, $strength, $dexterity, $constitution, $intelligence, $wisdom, $charisma);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deleteCharacterStats($stats_id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `stats` WHERE `stats_id` = ?");
+        $stmt->bind_param("i", $stats_id);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+
+
 ?>
