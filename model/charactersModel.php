814fb67839fb77746f7b64b74b5b6c2e035ce21f
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 23:30:42 -0600

    Update charactersModel.php

--- model/charactersModel.php
+++ model/charactersModel.php
@@ -1,1 +1,18 @@
-
+<?php
+function selectClasses() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT c.character_id, c.name, r.race, cl.class, c.level, c.alignment, c.background
+                                FROM characters c
+                                  JOIN races r ON c.char_race_id = r.race_id
+                                  JOIN classes cl ON c.char_class_id = cl.class_id);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
