e889df18170e596ae91dbb5660ff29a83cc784a7
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 06:41:24 -0600

    Update statsController.php

--- controller/statsController.php
+++ controller/statsController.php
@@ -12,4 +12,55 @@ $statsAndCharacters = selectStatsWithCharacters();
 include "/home/benfinmi/repositories/MIS4013_Final/view/statsView.php";
 
 include "/home/benfinmi/repositories/MIS4013_Final/Other/view-footer.php";
+
+
+if (isset($_POST['actionType'])) {
+    switch ($_POST['actionType']) {
+        case "Add":
+            $stats_char_id = $_POST['stats_char_id'];
+            $hit_points = $_POST['hit_points'];
+            $armor_class = $_POST['armor_class'];
+            $strength = $_POST['strength'];
+            $dexterity = $_POST['dexterity'];
+            $constitution = $_POST['constitution'];
+            $intelligence = $_POST['intelligence'];
+            $wisdom = $_POST['wisdom'];
+            $charisma = $_POST['charisma'];
+
+            if (insertCharacterStats($stats_char_id, $hit_points, $armor_class, $strength, $dexterity, $constitution, $intelligence, $wisdom, $charisma)) {
+                echo '<div class="alert alert-success" role="alert">Character Stats Added.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error</div>';
+            }
+            break;
+        case "Edit":
+            $stats_char_id = $_POST['stats_char_id'];
+            $hit_points = $_POST['hit_points'];
+            $armor_class = $_POST['armor_class'];
+            $strength = $_POST['strength'];
+            $dexterity = $_POST['dexterity'];
+            $constitution = $_POST['constitution'];
+            $intelligence = $_POST['intelligence'];
+            $wisdom = $_POST['wisdom'];
+            $charisma = $_POST['charisma'];
+
+            if (updateCharacterStats($stats_char_id, $hit_points, $armor_class, $strength, $dexterity, $constitution, $intelligence, $wisdom, $charisma)) {
+                echo '<div class="alert alert-success" role="alert">Character Stats Edited.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error</div>';
+            }
+            break;
+        case "Delete":
+            $stats_id = $_POST['stats_id'];
+
+            if (deleteCharacterStats($stats_id)) {
+                echo '<div class="alert alert-success" role="alert">Character Stats Deleted.</div>';
+            } else {
+                echo '<div class="alert alert-danger" role="alert">Error</div>';
+            }
+            break;
+    }
+}
+
+
 ?>
