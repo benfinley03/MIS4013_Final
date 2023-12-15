637bd1535b312580b56df4a0f8e12fce3b47bdd3
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 01:27:20 -0600

    Update statsController.php

--- controller/statsController.php
+++ controller/statsController.php
@@ -3,6 +3,6 @@ require_once("/home/benfinmi/repositories/MIS4013_Final/connection.php");
 require_once("/home/benfinmi/repositories/MIS4013_Final/model/statsModel.php");
 $pageTitle = "Character Stats";
 $cid = isset($_GET['id']) ? filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT) : null;
-$stats = selectStats($_GET['id']);
+$stats = selectStats($cid);
 include "/home/benfinmi/repositories/MIS4013_Final/view/statsView.php";
 ?>
