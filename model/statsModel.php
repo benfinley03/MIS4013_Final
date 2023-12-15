70c56ae2fbbac617ada5aced2b0ee9c8e1ce81fd
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 01:10:25 -0600

    Update statsModel.php

--- model/statsModel.php
+++ model/statsModel.php
@@ -3,8 +3,13 @@ function selectStats() {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT s.*, c.name, c.class 
-                                FROM stats s JOIN characters c ON s.stats_char_id = c.character_id");
+                                FROM stats s 
+                                    JOIN characters c ON s.stats_char_id = c.character_id");
 
+        if (!$stmt) {
+            die("Error in SQL query: " . $conn->error);
+        }
+        
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
