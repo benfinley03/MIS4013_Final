45e6ec8e99cd961137167b12f37e39d32f991461
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 23:40:43 -0600

    Update view-header.php

--- Other/view-header.php
+++ Other/view-header.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?php echo $pageTitle; ?></title>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Your custom CSS styles can be added here -->
+</head>
+<body>
+    
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
     <div class="container-fluid">
         <a class="navbar-brand" href="/index.php">My Character Database</a>
