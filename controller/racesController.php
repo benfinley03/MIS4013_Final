0e446db2976b0ac96084db7fb611bb948d8e93bf
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 03:18:26 -0600

    Update racesController.php

--- controller/racesController.php
+++ controller/racesController.php
@@ -3,6 +3,43 @@ require_once("/home/benfinmi/repositories/MIS4013_Final/connection.php");
 require_once("/home/benfinmi/repositories/MIS4013_Final/model/racesModel.php");
 $pageTitle = "Races";
 include "/home/benfinmi/repositories/MIS4013_Final/Other/view-header.php";
+
+
+
+if (isset($_POST['actionType'])) {
+  switch ($_POST['actionType']) {
+    case "Add":
+      if (insertRace($_POST['race']))
+      {
+        echo '<div class="alert alert-success" role="alert">Race Added.</div>';
+      } else 
+      {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Edit":
+      if (updateRace($_POST['race'], $_POST['race_id']))
+      {
+        echo '<div class="alert alert-success" role="alert">Race Edited.</div>';
+      } else 
+      {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+    case "Delete":
+      if (deleteRace($_POST['race_id'])) 
+      {
+        echo '<div class="alert alert-success" role="alert">Race Deleted.</div>';
+      } else 
+      {
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+  }
+}
+
+
+
 $races = selectRaces();
 include "/home/benfinmi/repositories/MIS4013_Final/view/racesView.php";
 include "/home/benfinmi/repositories/MIS4013_Final/Other/view-footer.php";
