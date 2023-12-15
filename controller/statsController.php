19c4a41b5dc6a12f2dbd44765f6aab968ce80d74
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 00:10:24 -0600

    Update statsController.php

--- controller/statsController.php
+++ controller/statsController.php
@@ -1,9 +1,7 @@
 <?php
 require_once("/home/benfinmi/repositories/MIS4013_Final/connection.php");
 require_once("/home/benfinmi/repositories/MIS4013_Final/model/statsModel.php");
-$pageTitle = "Characters";
-include "/home/benfinmi/repositories/MIS4013_Final/Other/view-header.php";
+$pageTitle = "Character Stats";
 $stats = selectStats($_POST['id']);
 include "/home/benfinmi/repositories/MIS4013_Final/view/statsView.php";
-include "/home/benfinmi/repositories/MIS4013_Final/Other/view-footer.php";
 ?>
