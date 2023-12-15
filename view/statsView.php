7034e80408ac45a2a744ab2e2814764407152343
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 04:54:34 -0600

    Update statsView.php

--- view/statsView.php
+++ view/statsView.php
@@ -21,3 +21,7 @@ while ($row = $statsAndCharacters->fetch_assoc()) {
 }
 
 ?>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-JFq4QqBEaayt2iGqDQsZTXB9tqPhkAUp3Pz+5PcEJ0tR3ZB/JmOrJSt5BO+dn+FZ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+Wy2nFgA5om8IbbVdJzazt+JX6p0a4U9Bp" crossorigin="anonymous"></script>
