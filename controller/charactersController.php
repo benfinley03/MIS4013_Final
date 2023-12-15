26f87a6b4b263a828f0ba7a4e1fde6c85ef41162
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 23:23:25 -0600

    Update charactersController.php

--- controller/charactersController.php
+++ controller/charactersController.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once("/home/benfinmi/repositories/MIS4013_Final/connection.php");
+require_once("/home/benfinmi/repositories/MIS4013_Final/model/charactersModel.php");
+$pageTitle = "Characters";
+include "/home/benfinmi/repositories/MIS4013_Final/Other/view-header.php";
+$characters = selectCharacters();
+include "/home/benfinmi/repositories/MIS4013_Final/view/charactersView.php";
+include "/home/benfinmi/repositories/MIS4013_Final/Other/view-footer.php";
+?>
