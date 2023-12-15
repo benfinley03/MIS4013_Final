180f66ad0c40122ef11da82c0972b754db7c88bb
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 01:56:56 -0600

    Update statsView.php

--- view/statsView.php
+++ view/statsView.php
@@ -1,6 +1,5 @@
 <h1>Character Stats</h1>
 
-<!-- Display Stats -->
 <div class="stats">
     <?php while ($char = $nameAndClass->fetch_assoc()) : ?>
         <div class="card">
@@ -9,7 +8,11 @@
                 <p><?php echo $char['class']; ?></p>
             </div>
             <div class="card-body">
-                <?php while ($stat = $stats->fetch_assoc()) : ?>
+                <?php
+                $charStats = selectStats($char['character_id']);
+                
+                while ($stat = $charStats->fetch_assoc()) :
+                ?>
                     <p>Character ID: <?php echo $stat['stats_char_id']; ?></p>
                     <p>Strength: <?php echo $stat['strength']; ?></p>
                     <p>Dexterity: <?php echo $stat['dexterity']; ?></p>
@@ -17,7 +20,9 @@
                     <p>Intelligence: <?php echo $stat['intelligence']; ?></p>
                     <p>Wisdom: <?php echo $stat['wisdom']; ?></p>
                     <p>Charisma: <?php echo $stat['charisma']; ?></p>
+                <?php endwhile; ?>
             </div>
         </div>
     <?php endwhile; ?>
 </div>
+
