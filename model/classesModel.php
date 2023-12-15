013bb813ae4058b4762b1eb2f4c9ad80420ca6d8
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 22:22:53 -0600

    Update classesModel.php

--- model/classesModel.php
+++ model/classesModel.php
@@ -1,1 +1,15 @@
-
+<?php
+function selectClasses() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT class_id, class FROM classes;");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
