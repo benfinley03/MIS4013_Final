b9de646d0edf687bd03a5d300014607c0934dabc
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 22:19:13 -0600

    Update index.php

--- Other/index.php
+++ Other/index.php
@@ -1,9 +1,9 @@
 <?php
 $pageTitle = "Home";
-include "Other/view-header.php";
+include "view-header.php";
 ?>
 <h1>Welcome to the My Character Database</h1>
 <h4>Please feel free to add, edit, or delete the classes, races, characters, and stats available!</h4>
 <?php
-include "Other/view-footer.php";
+include "view-footer.php";
 ?>
