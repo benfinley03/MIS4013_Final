607a05c1fe7d28b0d96b581d2e5ad52b5838bf95
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-14 23:15:41 -0600

    Update racesController.php

--- controller/racesController.php
+++ controller/racesController.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once("/home/benfinmi/repositories/MIS4013_Final/connection.php");
+require_once("/home/benfinmi/repositories/MIS4013_Final/model/racesModel.php");
+$pageTitle = "Classes";
+include "/home/benfinmi/repositories/MIS4013_Final/Other/view-header.php";
+$races = selectRaces();
+include "/home/benfinmi/repositories/MIS4013_Final/view/racesView.php";
+include "/home/benfinmi/repositories/MIS4013_Final/Other/view-footer.php";
+?>
