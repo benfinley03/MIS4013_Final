4565723ff2a143d70c8ca47b606eec2e5db9f82f
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 03:21:41 -0600

    Update racesModel.php

--- model/racesModel.php
+++ model/racesModel.php
@@ -13,3 +13,50 @@ function selectRaces() {
     }
 }
 ?>
+
+
+
+function insertRace($race) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `races` (`race`) VALUES (?)");
+        $stmt->bind_param("s", $race);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function updateRace($race, $race_id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("UPDATE `races` SET `race`=? WHERE race_id = ?");
+        $stmt->bind_param("si", $race, $race_id);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+
+function deleteRace($race_id) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("DELETE FROM `races` WHERE `race_id` = ?");
+        $stmt->bind_param("i", $race_id);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
