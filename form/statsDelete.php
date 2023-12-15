0e9f0e7c73b8f4efc3af81e6bf4b6ddf1ee2a763
benfinley03 <122949541+benfinley03@users.noreply.github.com>
2023-12-15 07:40:30 -0600

    Update statsDelete.php

--- form/statsDelete.php
+++ form/statsDelete.php
@@ -2,7 +2,7 @@
     <input type="hidden" name="stats_id" value="<?php echo $stats['stats_id']; ?>">
     <input type="hidden" name="actionType" value="Delete">
     <button type="submit" class="btn btn-danger" onclick="return confirm('Are you sure you want to delete?');">
-        Delete Stats
+        Delete
     </button>
 </form>
 
